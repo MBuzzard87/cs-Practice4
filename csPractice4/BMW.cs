d60b062e7bf8801c60ecefe10c2cd183e6bbc567
Michael Buzzard <m.buzzard87@gmail.com>
2021-10-28 17:52:47 -0400

    add bmw class

---
+++ csPractice4/BMW.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csPractice4
+{
+    class BMW : Car
+    {
+        public string Model { get; set; }
+        private string brand;
+        public BMW(int hp, string color, string model) : base(hp, color)
+        {
+            brand = "BMW";
+            Model = model;
+        }
+
+        public new void ShowDetails()
+        {
+            Console.WriteLine("The {0} {1} {2} has {3} HP", Color, brand, Model, HP);
+        }
+
+        public override void Repair()
+        {
+            Console.WriteLine("The {0} was not repaired because it cost too much to fix", brand);
+        }
+    }
+}
