f7052444715662dc38d17af68078f255b9a0dc89
Michael Buzzard <m.buzzard87@gmail.com>
2021-10-28 17:53:25 -0400

    polymorphism with car and audi/bmw

--- csPractice4/Program.cs
+++ csPractice4/Program.cs
@@ -10,6 +10,30 @@ namespace csPractice4
     {
         static void Main(string[] args)
         {
+            /* Classes:  Audi, BMW, Cars
+            var cars = new List<Car>
+            {
+                new Audi(300,"Silver", "A8"),
+                new BMW(330, "Black", "M3")
+            };
+
+            foreach(var car in cars)
+            {
+                car.ShowDetails();
+                car.Repair();
+            }
+            Console.WriteLine("\n\n");
+            Car bmw = new BMW(200, "Black", "Z3");
+            Console.WriteLine("Car type show details");
+            bmw.ShowDetails();
+
+            Console.WriteLine("\nBMW type show details");
+            BMW z3 = (BMW)bmw;
+            z3.ShowDetails();
+            */
+
+            Console.ReadKey();
+
         }
     }
 }
