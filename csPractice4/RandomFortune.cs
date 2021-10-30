ff9016efed5678674a2207d8c0111dfe2a288b6c
Michael Buzzard <m.buzzard87@gmail.com>
2021-10-30 19:57:28 -0400

    add randomfortune class

---
+++ csPractice4/RandomFortune.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csPractice4
+{
+    class RandomFortune
+    {
+
+        public RandomFortune()
+        {
+            Console.WriteLine("Ask a question and you shall receive your fortune");
+            Console.ReadLine();
+
+            Random numRan = new Random();
+            string[] fortune = { "Yes", "No", "Maybe" };
+
+            Console.WriteLine(fortune[numRan.Next(0, 3)]);
+
+            
+
+        }
+
+
+    }
+}
